e1e86a89bc1b6fa1477a3d7705b7520f42d641b9
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-19 00:31:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
    
   fido = Dog 
-  fido => # Dog:0x0000000001d3a288
+  fido => # => Dog:0x0000000001d3a288
     
   snoopy = Dog.new 
-  snoopy 
+  snoopy # => Dog:0x0000000001d52928
   
   lassie = Dog.new
-  lassie
+  lassie # => Dog:
   
 
 
